b094d6c0dc9aaff43ce0eca734fa2189dac10c1d
MelanyAC <arlinmelany@gmail.com>
2022-10-05 22:15:09 -0600

    Semana 02

---
+++ app/src/main/java/com/lugares/viewmodel/LugarViewModel.kt
@@ -0,0 +1,36 @@
+package com.lugares.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.lugares.data.LugarDao
+import com.lugares.data.LugarDatabase
+import com.lugares.model.Lugar
+import com.lugares.repository.LugarRepository
+import kotlinx.coroutines.launch
+
+class LugarViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: LugarRepository
+    val getLugares: LiveData<List<Lugar>>
+
+    init {
+
+        val lugarDao = LugarDatabase.getDatabase(application).lugarDao()
+        repository = LugarRepository(lugarDao)
+        getLugares = repository.getLugares
+
+    }
+
+    suspend fun addLugar(lugar: Lugar){
+        viewModelScope.launch { repository.addLugar(lugar)}
+    }
+
+
+
+    suspend fun updateLugar(lugar: Lugar) {
+        viewModelScope.launch { repository.updateLugar(lugar) }
+    }
+
+    suspend fun deleteLugar(lugar: Lugar) {
+        viewModelScope.launch { repository.deleteLugar(lugar) }
+    }
+}
